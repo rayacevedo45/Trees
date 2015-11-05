9ae5baf56b4b7c073f6e03d7c09b7fbe9a4fb459
rayacevdo45 <rayacevedo45@gmail.com>
2015-11-04 21:52:56 -0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.insert("5");
+        System.out.println("inserted 5");
+        tree.insert("4");
+        System.out.println("inserted 4");
+        tree.insert("2");
+        System.out.println("inserted 2");
+    }
+}
+
